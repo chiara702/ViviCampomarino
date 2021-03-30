f75eb4897441adcc3146d9e101c2f6139ea3ce80
HP <HP@ZBOOK15-FABIO>
2021-03-30 18:51:41 +0200

    ok

--- ViviCampomarino/Modulo.cs
+++ ViviCampomarino/Modulo.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+
 namespace ViviCampomarino {
     public class Database<T> {
         private IFirebaseFirestore current = CrossFirebaseFirestore.Current;
