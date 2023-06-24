5d3e492710482859cbe31e3f82e8d296ecc984af
dimariafabio <75421476+dimariafabio@users.noreply.github.com>
2023-06-24 07:45:23 +0200

    ok 31

---
+++ ViviCampomarino/ServizioNavetta/PageNavettaRegistrazione.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ViviCampomarino.ServizioNavetta {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageNavettaRegistrazione : ContentPage {
+        public PageNavettaRegistrazione() {
+            InitializeComponent();
+        }
+
+        private void BtnIndietro_Clicked(object sender, EventArgs e) {
+            Navigation.PopAsync();
+        }
+    }
+}
