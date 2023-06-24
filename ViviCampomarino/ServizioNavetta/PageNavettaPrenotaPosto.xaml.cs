783d692ad8bae7c4b6cd6cd57aea6e2b89674592
dimariafabio <75421476+dimariafabio@users.noreply.github.com>
2023-06-24 12:25:36 +0200

    Ok

--- ViviCampomarino/ServizioNavetta/PageNavettaPrenotaPosto.xaml.cs
+++ ViviCampomarino/ServizioNavetta/PageNavettaPrenotaPosto.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,33 @@ namespace ViviCampomarino.ServizioNavetta {
         public DateTime DataSelezionata;
         public PageNavettaPrenotaPosto(DateTime DataSelezionata) {
             InitializeComponent();
+            this.DataSelezionata = DataSelezionata;
             LblData.Text=DataSelezionata.ToString("dd/MM/yyyy");
+            //Verifica posti 
+            Task.Run(() => { 
+                var Db = new MySqlvc();
+                var Table = Db.EseguiQuery($"Select * From NavettaPrenotazioni Where Giorno='{DataSelezionata.ToString("yyyy-MM-dd HH:mm")}'");
+                Device.BeginInvokeOnMainThread(() => {
+                    foreach (DataRow x in Table.Rows) {
+                        if ((int)x["Posto"]==1) {
+                            LblPosto1.Text="Occupato";
+                        }
+                        if ((int)x["Posto"]==2) {
+                            LblPosto2.Text="Occupato";
+                        }
+                        if ((int)x["Posto"]==3) {
+                            LblPosto3.Text="Occupato";
+                        }
+
+                    }
+                });
+            });
         }
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e) {
-            DisplayAlert("", sender.ToString() + DataSelezionata.ToLongDateString(), "ok");
+            var Posto = Convert.ToInt16(((TappedEventArgs)e).Parameter);
+            var form = new PageNavettaRegistrazione(DataSelezionata, Posto);
+            Navigation.PushAsync(form); 
         }
 
         private void BtnIndietro_Clicked(object sender, EventArgs e) {
--- ViviCampomarino/ServizioNavetta/PageNavettaRegistrazione.xaml.cs
+++ ViviCampomarino/ServizioNavetta/PageNavettaRegistrazione.xaml.cs
@@ -6,15 +6,118 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Xaml.Diagnostics;
+using ZXing;
 
 namespace ViviCampomarino.ServizioNavetta {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PageNavettaRegistrazione : ContentPage {
-        public PageNavettaRegistrazione() {
+        private DateTime DataSelezionata;
+        private int Posto;
+        private int IdPrenotazione = 0;
+        public PageNavettaRegistrazione(DateTime DataSelezionata, int Posto) {
             InitializeComponent();
+            this.DataSelezionata=DataSelezionata;
+            this.Posto=Posto;
+            LblData.Text = DataSelezionata.ToString("dd/MM/yyyy HH:mm");
+            var Db = new MySqlvc();
+            var rowP = Db.EseguiRow($"Select * from NavettaPrenotazioni where Giorno='{DataSelezionata.ToString("yyyy-MM-dd HH:mm")}' and Posto={Posto}");
+            Db.CloseCommit();
+            if (rowP != null) {
+                if (rowP["GuidDevice"].ToString()==App.Guid) {
+                    BtnAnnullaPrenotazione.IsVisible=true;
+                    BtnAnnulla.IsVisible=false;
+                    IdPrenotazione=(int)rowP["Id"];
+                    TxtNome.Text=rowP["Nome"].ToString();
+                    TxtTelefono.Text=rowP["Telefono"].ToString();
+
+                }
+            }
         }
 
         private void BtnIndietro_Clicked(object sender, EventArgs e) {
+            Navigation.PopAsync();
+        }
+
+        private void SwitchAccompagnatore_Toggled(object sender, ToggledEventArgs e) {
+            StkAccompagnatore.IsVisible=e.Value;
+        }
+
+        private void BtnAnnulla_Clicked(object sender, EventArgs e) {
+
+        }
+
+        private void BtnConferma_Clicked(object sender, EventArgs e) {
+            //Verifica dati principali
+            if (String.IsNullOrEmpty(TxtNome.Text)==true) {
+                DisplayAlert("Errore", "Occorre inserire il nome!", "ok");
+                return;
+            }
+            if (String.IsNullOrEmpty(TxtIndirizzoPrelievo.Text)==true) {
+                DisplayAlert("Errore", "Occorre inserire indirizzo di prelievo/partenza!", "ok");
+                return;
+            }
+            if (String.IsNullOrEmpty(TxtIndirizzoDestinazione.Text)==true) {
+                DisplayAlert("Errore", "Occorre inserire indirizzo di rilascio/destinazione!", "ok");
+                return;
+            }
+            if (SwitchAccompagnatore.IsToggled==true && string.IsNullOrEmpty(TxtAccompagnatoreNome.Text)==true) {
+                DisplayAlert("Errore", "Occorre inserire almeno il nome dell'accomapagnatore", "ok");
+                return;
+            }
+            //Verifica posto già occupato
+            var Db = new MySqlvc();
+            var rowP=Db.EseguiRow($"Select * from NavettaPrenotazioni where Giorno='{DataSelezionata.ToString("yyyy-MM-dd HH:mm")}' and Posto={Posto}");
+            Db.CloseCommit();
+            if (rowP != null) {
+                DisplayAlert("Errore", $"Posto già prenotato da {rowP["GuidDevice"]}! Provare con un altro posto o altro orario.", "ok");
+                return;
+            }
+            //Salva prenotazione
+            var SalvaOk = 0;
+            try { 
+                Db = new MySqlvc();
+                var bis = new MySqlvc.DBSqlBis(Db, "NavettaPrenotazioni");
+                bis.GetParam.AddWithValue("Giorno", DataSelezionata);
+                bis.GetParam.AddWithValue("Posto", Posto);
+                bis.GetParam.AddWithValue("GuidDevice", App.Guid);
+                bis.GetParam.AddWithValue("GuidDevice", App.Guid);
+                bis.GetParam.AddWithValue("Nome", Funzioni.Antinull(TxtNome.Text));
+                bis.GetParam.AddWithValue("Telefono", Funzioni.Antinull(TxtTelefono.Text));
+                bis.GetParam.AddWithValue("Accompagnatore", SwitchAccompagnatore.IsToggled);
+                if (SwitchAccompagnatore.IsToggled==false) { TxtAccompagnatoreNome.Text=""; TxtAccompagnatoreTelefono.Text=""; }
+                bis.GetParam.AddWithValue("NomeAccompagnatore", Funzioni.Antinull(TxtAccompagnatoreNome.Text));
+                bis.GetParam.AddWithValue("TelefonoAccompagnatore", Funzioni.Antinull(TxtAccompagnatoreTelefono.Text));
+                bis.GetParam.AddWithValue("IndirizzoPresa", Funzioni.Antinull(TxtIndirizzoPrelievo.Text));
+                bis.GetParam.AddWithValue("IndirizzoRilascio", Funzioni.Antinull(TxtIndirizzoDestinazione.Text));
+                bis.GetParam.AddWithValue("Note", Funzioni.Antinull(TxtNote.Text));
+                if (IdPrenotazione==0) {
+                    bis.GetParam.AddWithValue("DataOraCreazione", DateTime.Now);
+                    bis.GetParam.AddWithValue("DataOraModifica", DateTime.Now);
+                    bis.GeneraInsert();
+                    Db.CloseCommit();
+                } else {
+                    bis.GetParam.AddWithValue("DataOraModifica", DateTime.Now);
+                    bis.GeneraUpdate(IdPrenotazione);
+                    Db.CloseCommit();
+                }
+                SalvaOk=1;
+            } catch(Exception ex) {
+                DisplayAlert("Errore", "Prenotazione non registrata a causa di un problema di connessione!", "Ok");
+            }
+            if (SalvaOk==1) { Navigation.PopAsync(); }
+
+        }
+
+        private void BtnAnnullaPrenotazione_Clicked(object sender, EventArgs e) {
+            try {
+                var Db = new MySqlvc();
+                Db.EseguiScalare("Delete From NavettaPrenotaqzioni Where Id=" + IdPrenotazione);
+                Db.CloseCommit();
+            } catch(Exception ex) {
+                DisplayAlert("Errore", "Prenotazione non annullata a causa di un problema di connessione!", "Ok");
+            }
+            DisplayAlert("Prenotazione", "Prenotazione annullata con successo!", "Ok");
             Navigation.PopAsync();
         }
     }
