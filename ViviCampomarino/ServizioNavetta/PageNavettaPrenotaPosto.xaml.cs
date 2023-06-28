fded7ff3d825d5cfaacdc78ed077df1f0367dab2
dimariafabio <75421476+dimariafabio@users.noreply.github.com>
2023-06-28 06:28:51 +0200

    ok

--- ViviCampomarino/ServizioNavetta/PageNavettaPrenotaPosto.xaml.cs
+++ ViviCampomarino/ServizioNavetta/PageNavettaPrenotaPosto.xaml.cs
@@ -37,10 +37,13 @@ namespace ViviCampomarino.ServizioNavetta {
                 var Db = new MySqlvc();
                 var Table = Db.EseguiQuery($"Select * From NavettaPrenotazioni Where Giorno='{DataSelezionata.ToString("yyyy-MM-dd HH:mm")}'");
                 Device.BeginInvokeOnMainThread(() => {
+                    SetOccupatoLibero(LblPosto1, ImgPosto1, false);
+                    SetOccupatoLibero(LblPosto2, ImgPosto2, false);
+                    SetOccupatoLibero(LblPosto3, ImgPosto3, false);
                     foreach (DataRow x in Table.Rows) {
-                        SetOccupatoLibero(LblPosto1, ImgPosto1, (int)x["Posto"]==1);
-                        SetOccupatoLibero(LblPosto2, ImgPosto2, (int)x["Posto"]==2);
-                        SetOccupatoLibero(LblPosto3, ImgPosto3, (int)x["Posto"]==3);
+                        if ((int)x["Posto"]==1) SetOccupatoLibero(LblPosto1, ImgPosto1, true);
+                        if ((int)x["Posto"]==2) SetOccupatoLibero(LblPosto2, ImgPosto2, true);
+                        if ((int)x["Posto"]==3) SetOccupatoLibero(LblPosto3, ImgPosto3, true);
                     }
                 });
             });
@@ -52,14 +55,17 @@ namespace ViviCampomarino.ServizioNavetta {
             var Posto = Convert.ToInt16(((TappedEventArgs)e).Parameter);
             //Controllo se prenotazione già eseguita su altro posto
             var Db = new MySqlvc();
-            var rowP = Db.EseguiRow($"Select * from NavettaPrenotazioni where Giorno='{DataSelezionata.ToString("yyyy-MM-dd HH:mm")}' And Posto={Posto}");
+            var TablePrenGiorno = Db.EseguiQuery($"Select * from NavettaPrenotazioni where Giorno='{DataSelezionata.ToString("yyyy-MM-dd HH:mm")}'");
             Db.CloseCommit();
-            if (rowP !=null && (int)rowP["Posto"]!=Posto) {
-                DisplayAlert("Prenotazione", "Hai già prenotato!", "ok");
+            //if (rowP !=null && (int)rowP["Posto"]!=Posto) {
+            if (TablePrenGiorno.Select($"GuidDevice='{App.Guid}' And Posto<>{Posto}").Count()>0) {
+                DisplayAlert("Prenotazione", "Hai già prenotato su altro posto!", "ok");
                 return;
             }
-            
-            
+            if (TablePrenGiorno.Select($"Posto={Posto} And GuidDevice<>'{App.Guid}'").Count()>0) {
+                DisplayAlert("Prenotazione", "Posto già prenotato. Provare con uno libero!", "ok");
+                return;
+            }
             //
             var form = new PageNavettaRegistrazione(DataSelezionata, Posto);
             Navigation.PushAsync(form); 
